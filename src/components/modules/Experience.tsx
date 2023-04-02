4ed55947220617b0643d0c35819bd2192a1f52ca
Kermit2626 <oligray26@gmail.com>
2023-04-02 21:35:01 +0100

    projects

---
+++ src/components/modules/Experience.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+// todo experience and skills 
+
+const Experience:FC = () => {
+    return (
+        <div></div> 
+    )
+}
+
+export default Experience;
---
+++ src/components/modules/Projects.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Projects:FC = () => {
+    return(
+        <div>
+            
+
+        </div>
+    )
+}
+
+export default Projects;
